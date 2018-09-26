105faac58e120de533e9713ee3e3326a87c2c357
CesarAdan1 <whoclash@hotmail.com>
2018-09-26 10:02:30 -0500

    test2

--- src/models/Companies.js
+++
@@ -1,9 +0,0 @@
-const ODM = require('mongoose');
-
-const Schema = ODM.Schema({
-    _id: ODM.Schema.Types.ObjectId,
-    name: String
-});
-
-module.exports = ODM.model('Companies', Schema);
-
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,40 +1,13 @@
+
 const { Router } = require('express');
-
-/* Initialise and create a Router instance */
 const app = Router();
-
-/**
- * Controllers
- *
- * They will help us to write our logic in a separated file to keep clean
- * our routes configuration file.
- *
- * [1] Companies
- */
-const Companies = require('../controllers/Companies');
 const User = require('../controllers/User');
 
-const isAuthenticated = require('../services/Auth');
-
-/**
- * Endpoints
- *
- * [1] Companies
- *     `get(/companies)`
- *     `post(/companies)`
- *     `put(/companies)`
- *     `delete(/companies)`
- */
-app.route('/companies')
-  .get(Companies.index)
-  .post(isAuthenticated, Companies.create)
-  .put(Companies.update)
-  .delete(Companies.remove);
-
-app.get('/companies/:companyId', Companies.getById);
-
+app.get('/users', User.index);
+app.delete('/users/:userId', User.remove);
 app.post('/auth/signup', User.create);
 app.post('/auth/login', User.login);
-app.get('/users', User.index);
+
+
 
 module.exports = app;
